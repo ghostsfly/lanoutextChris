9e84ec24b537b3df30efba4898d892d20e4199bf
ghostsfly <1960732609@qq.com>
2016-03-09 11:35:17 +0800

    huh

---
+++ 工程管理/工程管理/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  工程管理
+//
+//  Created by Chris on 16/3/9.
+//  Copyright © 2016年 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 工程管理/工程管理/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  工程管理
+//
+//  Created by Chris on 16/3/9.
+//  Copyright © 2016年 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
